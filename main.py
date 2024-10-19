5bcde89d2be3aa21a2da4c32b272f95593de619a
Bennasrabdelmomen <123730041+Bennasrabdelmomen@users.noreply.github.com>
2024-10-19 11:32:10 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
 import praw
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-import re
+import os
 
-with open('unique_valid_reddit_urls.txt', 'r', encoding='utf-8') as file:
-    valid_links = [line.strip() for line in file.readlines()]
-
-reddit = praw.Reddit(user_agent=True, client_id="sss ",
+reddit = praw.Reddit(user_agent=True, client_id="ssss",
                      client_secret="sss",
                      user_name="sss", password="sss")
+folder_path = 'reddit_crawled_urls'
+file_counter = 1
+for filename in os.listdir(folder_path):
+    if filename.endswith(".txt"):
+        file_path = os.path.join(folder_path, filename)
 
-posts_data = []
+        with open(file_path, 'r', encoding='utf-8') as file:
+            valid_links = [line.strip() for line in file.readlines()]
 
-for full_url in valid_links:
-    try:
-        post = reddit.submission(url=full_url)
+        posts_data = []
 
-        post.comments.replace_more(limit=0)
-        comments = [comment.body for comment in post.comments.list()]
+        for full_url in valid_links:
+            try:
+                post = reddit.submission(url=full_url)
 
-        post_info = {
-            "title": post.title,
-            "selftext": post.selftext,
-            "num_comments": post.num_comments,
-            "comments": comments
-        }
+                post.comments.replace_more(limit=0)
+                comments = [comment.body for comment in post.comments.list()]
 
-        posts_data.append(post_info)
+                post_info = {
+                    "title": post.title,
+                    "post_content": post.selftext,
+                    "num_comments": post.num_comments,
+                    "comments": comments
+                }
 
-    except Exception as e:
-        print(f"An error occurred while processing {full_url}: {e}")
+                posts_data.append(post_info)
 
-df = pd.DataFrame(posts_data)
-#df = df.drop_duplicates()
+            except Exception as e:
+                print(f"An error occurred while processing {full_url}: {e}")
 
-df.to_csv("reddit_posts.csv", index=False)
-print("Posts and comments saved to 'reddit_posts.csv'.")
+        df = pd.DataFrame(posts_data)
+
+        df.to_csv(f"df{file_counter}.csv", index=False)
+        print(f"Dataset saved to 'df{file_counter}.csv'.")
+
+        file_counter += 1
--- main2.py
+++ main2.py
@@ -1,37 +1,49 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+import os
 
-
-
-##get data from x link on reddit and store in txt file to be fed into le other main
 driver = webdriver.Chrome()
 
-url = "https://www.reddit.com/search/?q=%D8%A7%D9%84%D8%A7%D9%83%D8%AA%D8%A2%D8%A8&cId=c7c7e9b5-0f76-43a1-94dc-c2d45c31ede1&iId=8c9b6649-a4d5-4a29-a0af-73e6e9ed98be"
-driver.get(url)
+with open("url_keywords.txt", "r", encoding="utf-8") as file:
+    keywords_url = [line.strip() for line in file.readlines()]
 
-scroll_pause_time = 2
-screen_height = driver.execute_script("return window.screen.height;")
-i = 1
+output_folder = 'reddit_crawled_urls'
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
-while True:
-    driver.execute_script(f"window.scrollTo(0, {screen_height * i});")
-    i += 1
-    time.sleep(scroll_pause_time)
 
-    scroll_height = driver.execute_script("return document.body.scrollHeight;")
-    if screen_height * i > scroll_height:
-        break
+file_counter = 1
+for url in keywords_url:
+    driver.get(url)
+    scroll_pause_time = 2
+    screen_height = driver.execute_script("return window.screen.height;")
+    i = 1
 
-soup = BeautifulSoup(driver.page_source, "html.parser")
+    while True:
+        driver.execute_script(f"window.scrollTo(0, {screen_height * i});")
+        i += 1
+        time.sleep(scroll_pause_time)
 
-posts = soup.find_all('a', href=True)
+        scroll_height = driver.execute_script("return document.body.scrollHeight;")
+        if screen_height * i > scroll_height:
+            break
 
-post_urls = [post['href'] for post in posts if '/r/' in post['href']]
+    soup = BeautifulSoup(driver.page_source, "html.parser")
 
-with open("reddit_results.txt", "w", encoding="utf-8") as file:
-    for url in post_urls:
-        file.write("https://www.reddit.com" + url + "\n")
+    posts = soup.find_all('a', href=True)
+
+    post_urls = [post['href'] for post in posts if '/r/' in post['href']]
+
+    file_name = f"urls_to_crawl{file_counter}.txt"
+    file_path = os.path.join(output_folder, file_name)
+
+    with open(file_path, "w", encoding="utf-8") as file:
+        for post_url in post_urls:
+            file.write("https://www.reddit.com" + post_url + "\n")
+
+    file_counter += 1
+
 driver.quit()
 
-print("Results saved in reddit_results.txt")
+print(f"Results saved in the folder: {output_folder}")
